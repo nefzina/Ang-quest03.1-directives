62e7f1794aa0e1b657ebea9df05808fa19ad24aa
nefzina <nefzi.amani@gmail.com>
2023-09-20 16:27:09 +0200

    quest 03.1 - directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'CodeSandbox';
+  title: string = 'Welcome to Angular Quest 03.1';
+  isAdmin: boolean = false;
+
+  toggle():void {
+    this.isAdmin = !this.isAdmin;
+  }
 }
